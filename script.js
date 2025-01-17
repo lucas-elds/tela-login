690a52a0a45a48361c054ef44907aa28e7703403
Lucas <lucasemiliano619@gmail.com>
2025-01-17 19:56:52 -0300

    Validação de Email e Senha
    Uso de eventListeners e funções para validar os campos do formulário, verificando o formato do e-mail e realizando uma checagem no tamanho da senha.

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const campoEmail = document.querySelector("#email");
+const campoSenha = document.querySelector("#senha");
+
+campoEmail.addEventListener("keyup", validarEmail);
+campoSenha.addEventListener("keyup", validarSenha);
+
+const dicaEmail = document.querySelector(".email");
+const dicaSenha = document.querySelector(".senha");
+
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+function validarEmail() {
+    if (campoEmail.value == "") {
+        dicaEmail.innerHTML = "Este campo não pode ficar vazio.";
+    } else if (regexEmail.test(campoEmail.value)) {
+        dicaEmail.innerHTML = "";
+    } else {
+        dicaEmail.innerHTML = "Formato de e-mail inválido";
+    }
+}
+
+function validarSenha() {
+    if (campoSenha.value == "") {
+        dicaSenha.innerHTML = "Este campo não pode ficar vazio.";
+    } else if (campoSenha.value.length >= 6) {
+        dicaSenha.innerHTML = "";
+    } else {
+        dicaSenha.innerHTML = "Insira pelo menos 6 caracteres.";
+    }
+}
+
 function entrar() {
 
 }
